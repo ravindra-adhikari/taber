5299901310cfd2e07a7acc4b3ccc833a3927ccf2
ravindra_adhikari <z@ravindra.net.np>
2019-09-19 15:48:22 +0200

    server set up with express and other dependencies

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+console.log(`hello form server`);
+
+const express = require('express')
+const cors = require ('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 5000)
+
+app.get('/status',(req, res) => {
+    res.send({
+        message: 'hello world !'
+    })
+})
