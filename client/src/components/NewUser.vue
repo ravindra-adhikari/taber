fc45786866a059a58379b5d99cd2e41b29768188
ravindra_adhikari <z@ravindra.net.np>
2019-10-02 00:56:44 +0200

    now client side is in vue cli not webpack, and vueitfy manually added.

---
+++ client/src/components/NewUser.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container grid-list-xs>
+    <v-layout>
+        <v-flex>
+            <v-card>
+            <v-toolbar flat>
+                <v-toolbar-title>
+                    Register
+                </v-toolbar-title>
+
+            </v-toolbar>
+            <v-card-text>
+                <v-form>
+                    <v-text-field
+                        name="email"
+                        label="email"
+                        id="email"
+                        v-model="email"
+                    ></v-text-field>
+                    <v-text-field
+                        name="password"
+                        label="Password"
+                        id="password"
+                        :type= "password"
+                        v-model="password"
+                    ></v-text-field>
+                    <v-btn rounded color="primary" dark>Register Now !</v-btn>
+                </v-form>
+            </v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'NewUser',
+    data () {
+        return{
+            email: 'me@domain.com',
+            password: 'password'
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <NewUser />
+</template>
+
+<script>
+import NewUser from '@/components/NewUser.vue'
+export default {
+name:'login',
+components: {
+    NewUser 
+    }
+}
+</script>
+
+<style>
+
+</style>
